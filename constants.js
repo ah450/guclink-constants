7f0e0c4a3797bb622570090ec5246dfad0a5b5ee
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2016-04-04 05:39:54 +0200

    Fixes issue with urls

--- constants.js
+++ constants.js
@@ -1,11 +1,11 @@
 angular.module('guclinkConstants', []);
 
 angular.module('guclinkConstants')
-.constant('AUTH_BASE_URL', 'auth.guclink.in')
-.constant('ORGANIZER_BASE_URL', 'organizer.guclink.in')
-.constant('ATTENDANCE_BASE_URL', 'attendance.guclink.in')
-.constant('NAV_BASE_URL', 'nav.guclink.in')
-.constant('CLASS_BASE_URL', 'class.guclink.in')
-.constant('EXAM_BASE_URL', 'exam.guclink.in')
-.constant('PARKING_BASE_URL', 'parking.guclink.in')
-.constant('DATA_BASE_URL', 'data.guclink.in');
+.constant('AUTH_BASE_URL', 'auth.guclink.in/api')
+.constant('ORGANIZER_BASE_URL', 'organizer.guclink.in/api')
+.constant('ATTENDANCE_BASE_URL', 'attendance.guclink.in/api')
+.constant('NAV_BASE_URL', 'nav.guclink.in/api')
+.constant('CLASS_BASE_URL', 'class.guclink.in/api')
+.constant('EXAM_BASE_URL', 'exam.guclink.in/api')
+.constant('PARKING_BASE_URL', 'parking.guclink.in/api')
+.constant('DATA_BASE_URL', 'data.guclink.in/api');
