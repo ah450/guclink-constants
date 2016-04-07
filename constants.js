a110aaf3e31790238bef990846073b8197fe8f5a
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2016-04-07 16:35:18 +0200

    Adds protocol to constants

--- constants.js
+++ constants.js
@@ -1,11 +1,11 @@
 angular.module('guclinkConstants', []);
 
 angular.module('guclinkConstants')
-.constant('AUTH_BASE_URL', 'auth.guclink.in/api')
-.constant('ORGANIZER_BASE_URL', 'organizer.guclink.in/api')
-.constant('ATTENDANCE_BASE_URL', 'attendance.guclink.in/api')
-.constant('NAV_BASE_URL', 'nav.guclink.in/api')
-.constant('CLASS_BASE_URL', 'class.guclink.in/api')
-.constant('EXAM_BASE_URL', 'exam.guclink.in/api')
-.constant('PARKING_BASE_URL', 'parking.guclink.in/api')
-.constant('DATA_BASE_URL', 'data.guclink.in/api');
+.constant('AUTH_BASE_URL', 'https://auth.guclink.in/api')
+.constant('ORGANIZER_BASE_URL', 'https://organizer.guclink.in/api')
+.constant('ATTENDANCE_BASE_URL', 'https://attendance.guclink.in/api')
+.constant('NAV_BASE_URL', 'https://nav.guclink.in/api')
+.constant('CLASS_BASE_URL', 'https://class.guclink.in/api')
+.constant('EXAM_BASE_URL', 'https://exam.guclink.in/api')
+.constant('PARKING_BASE_URL', 'https://parking.guclink.in/api')
+.constant('DATA_BASE_URL', 'https://data.guclink.in/api');
